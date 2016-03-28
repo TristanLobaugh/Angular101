0cd1cb001e9c24eb2faed641fc31157b8e67510a
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-28 10:08:19 -0400

    init commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+// PLAIN Javascript WAY
+// var myInput = document.getElementById("myInput");
+
+// myInput.addEventListener("keyup", function(){
+// 	console.log("User hit a key!");
+// 	var userText = myInput.value;
+// 	document.getElementById("inputText").innerHTML = userText;
+// });
+
+
+// THE jQuery WAY
+// $("#myInput").keyup(function(){
+// 	$("#inputText").html($("#myInput").val());
+// });
